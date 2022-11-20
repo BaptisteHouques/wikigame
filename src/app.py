348ca99144b2c006e8208eef54e783e06a28de05
BaptisteHouques <baptistehouques38@gmail.com>
2022-11-20 17:24:14 +0100

    fix & clean

--- src/app.py
+++ src/app.py
@@ -1,11 +1,8 @@
 from tkinter import *
 from function.search import search
-from function.getLinks import getLinks
 from function.getPageTitle import getPageTitle
-from function.showLinks import showLinks
 from function.changeUrl import changeUrl
 from function.countdown import countdown
-import time
 
 # Création de la fenêtre
 window = Tk()
@@ -29,18 +26,16 @@ label_tour = Label(window, text="Tour : 0")
 label_tour.pack()
 
 # Création timer
-timeValue = 5
+timeValue = 600 # Temps imparti (en seconde)
 timeString = StringVar()
 countdownBox = Label(window, textvariable=timeString)
 countdownBox.pack()
 
 Label(window, text="-"*30).pack()
 
-# Nombre maximum de liens affichés
-max = 15
-
 # Création boite des liens & affichage de la page actuelle et de ses liens
 frame_links = Frame(window)
+max = 15 # Nombre maximum de liens affichés
 changeUrl("/wiki/Sp%C3%A9cial:Page_au_hasard", label_actuel, frame_links, label_tour, max, cible)
 frame_links.pack()
 
